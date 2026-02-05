2ffd04569fac1913f40bd556c4ffdfdefd38c019
omegadevelopmentsfr <omegadevelopmentsfr@gmail.com>
2026-02-05 20:54:03 +0100

    fix: update basePath and assetPrefix to use hyphenated project name

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/Michelet_Robotique' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/Michelet_Robotique/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/Michelet-Robotique' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Michelet-Robotique/' : '',
     images: {
         unoptimized: true,
     },
