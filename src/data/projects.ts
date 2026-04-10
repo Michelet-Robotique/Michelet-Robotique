12ca0c0a505c879dfa2842bb363a2feffddef0a0
Omega Developments <omegadevelopmentsfr@gmail.com>
2026-04-10 11:49:31 +0200

    feat: add projects data structure and update gitignore to track configuration file

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,27 +9,20 @@ export interface Project {
 export const projects: Project[] = [
     {
         id: 1,
-        title: "Projet Coupe de Robotique 2026",
-        description: "Robot qui participera à la coupe de robotique 2026.",
-        imageUrl: "/assets/project1.jpg",
-        link: "#"
-    },
-    {
-        id: 2,
         title: "Projet Poubelle",
         description: "Robot poubelle qui intercepte les déchets en vol",
         imageUrl: "/assets/project2.jpg",
         link: "#"
     },
     {
-        id: 3,
+        id: 2,
         title: "Projet Karting Électrique",
         description: "Conception et réalisation d'un kart électrique, avec une motorisation optimisée.",
         imageUrl: "/assets/project3.jpg",
         link: "#"
     },
     {
-        id: 4,
+        id: 3,
         title: "Plus de projets à venir !",
         description: "Le club de robotique ne s'arrête jamais. Restez à l'écoute pour nos prochaines créations !",
         imageUrl: "/assets/project4.jpg",
